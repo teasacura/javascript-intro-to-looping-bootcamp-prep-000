fe88484f3a8c9e4e75cdb828813b74c194e3c3d5
Eva Leake <teasacuraster@gmail.com>
2018-03-06 17:18:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,7 @@ return Math.random() >= 0.5
 
 function doWhileLoop(array) {
 do {
-  array = array.length - 1
+  array.length - 1 = array
 } while (array.length > 0 && maybeTrue());
 return array
 }
