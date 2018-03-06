3b2932f3d8d8c6a11e0aebd9b3f25f03a49afe67
Eva Leake <teasacuraster@gmail.com>
2018-03-06 17:04:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array) {
   for (let i = 1; i < 25; i++) {
     if (i === 1) {
-      console.log("I am 1 strange loop.")
+      console.log(`I am 1 strange loop.`)
     } else {
       console.log(`I am ${i} strange loops.`)
     }
